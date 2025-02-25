12e47383ef92e656d1448b4e18d603913e0ee265
louaila <lou591@hotmail.fr>
2025-02-25 16:17:17 +0100

    addFunctionGotohome

--- tests/decathlon.categorie.spec.ts
+++ tests/decathlon.categorie.spec.ts
@@ -1,18 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+
+async function goToHomePageAndAcceptCookies(page) {
   await page.goto('https://www.decathlon.fr/');
   await page.getByRole('button', { name: 'Accepter & Fermer: Accepter' }).click();
+}
+
+
+test('categorieLutte', async ({ page }) => {
+  await goToHomePageAndAcceptCookies(page);
   await page.getByRole('searchbox', { name: 'Rechercher un produit, un' }).click();
   await page.getByRole('searchbox', { name: 'Rechercher un produit, un' }).fill('lutte');
   await page.getByRole('searchbox', { name: 'Rechercher un produit, un' }).press('Enter');
 });
 
 
-test('test2', async ({ page }) => {
-  await page.goto('https://www.decathlon.fr/');
-  await page.getByRole('button', { name: 'Accepter & Fermer: Accepter' }).click();
+test('categorieFemme', async ({ page }) => {
+  await goToHomePageAndAcceptCookies(page);
   await page.getByRole('menuitem', { name: 'Femme' }).click();
   await page.getByRole('menuitem', { name: 'Se protéger du froid' }).click();
 
+});
+
+
+test('categorieHomme', async ({ page }) => {
+  await goToHomePageAndAcceptCookies(page);
+  await page.getByRole('menuitem', { name: 'Homme' }).click();
+  await page.getByRole('banner').getByRole('link', { name: 'Chaussures' }).click();
 });
